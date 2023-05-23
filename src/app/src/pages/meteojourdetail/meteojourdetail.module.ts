355995ac0c8fe775ac1922759c4c150c559cadb4
lamourette antoine <antoine.lamourette59@gmail.com>
2023-05-23 14:13:36 +0200

    rajout de component + module

---
+++ src/app/src/pages/meteojourdetail/meteojourdetail.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeteojourdetailRoutingModule } from './meteojourdetail-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MeteojourdetailRoutingModule
+  ]
+})
+export class MeteojourdetailModule { }
---
+++ src/app/src/pages/meteoseptjour/meteoseptjour.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeteoseptjourRoutingModule } from './meteoseptjour-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MeteoseptjourRoutingModule
+  ]
+})
+export class MeteoseptjourModule { }
