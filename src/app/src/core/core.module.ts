f76d91fbdaeb55591da836b5932b66ff7d64c7cd
lamourette antoine <antoine.lamourette59@gmail.com>
2023-05-24 10:47:05 +0200

    amelioration routing

--- src/app/src/core/core.module.ts
+++ src/app/src/core/core.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {FooterComponent} from 'src/app/src/core/layout/footer/footer.component'
+import { SlidebarComponent } from './layout/slidebar/slidebar.component'
+
 import { RouterModule } from '@angular/router';
 
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    SlidebarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
+  ],
+  exports: [
+    SlidebarComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
